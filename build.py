750d76f0aa5133d2a84cc7972178ae2b30c1db0f
Frieder Pankratz <frieder.pankratz@gmail.com>
2021-05-01 19:39:22 +0200

    fix job

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(pure_c=True, shared_option_name=False)
+    #builder.add_common_builds(pure_c=True, shared_option_name=False)
+    builder.add(settings={"build_type": "Debug"}, options={}, env_vars={}, build_requires={})
+    builder.add(settings={"build_type": "Release"}, options={}, env_vars={}, build_requires={})
     
     builder.run()    
